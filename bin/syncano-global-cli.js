61d7431e172c80842979224b24ec521b0c44b158
Maciej Korsan <maciejkorsan@gmail.com>
2018-02-01 21:11:41 +0100

    fix: back to npx

--- bin/syncano-global-cli.js
+++ bin/syncano-global-cli.js
@@ -1,27 +1,14 @@
-#!/usr/bin/env node
-const fs = require('fs');
+#!/usr/bin/env node 
 const { spawn } = require("child_process");
 
 
-const path = process.cwd();
-let syncanoPath = './node_modules/.bin/s';
-
-function runCMD(binary) {
-  const subprocess = spawn(`node ${binary}`, process.argv.slice(2), {
-    argv0: path,
-    shell: true,
-    stdio: "inherit"
-  });
-}
-
 console.log('Running with syncano-global-cli 🌍');
 
-if (fs.existsSync(syncanoPath)) {
-  runCMD(syncanoPath);
-} else if (fs.existsSync('./.'+syncanoPath)) {
-  runCMD('./.'+syncanoPath);
-} else if (fs.existsSync('./../.'+syncanoPath)) { 
-  runCMD('./../.'+syncanoPath);
-} else {
-  console.log('syncano-cli not found')
-} 
+const path = process.cwd(); 
+ 
+  const subprocess = spawn(`npx s`, process.argv.slice(2), {
+    argv0: path,
+    cwd: path,
+    shell: true,
+    stdio: "inherit"
+  }); 
